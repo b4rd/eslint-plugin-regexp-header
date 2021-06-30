6af687e36cd44f4e833d6960e8cc47661ec46df3
Peter Jakkel <peter.jakkel@zeiss.com>
2021-06-30 23:45:07 +0200

    Add support for inline regex

--- tests/lib/rules/regexp-header.js
+++ tests/lib/rules/regexp-header.js
@@ -1,15 +1,41 @@
 "use strict";
 
-const rule = require('../../../lib/rules/regexp-header');
-const RuleTester = require('eslint').RuleTester;
+const rule = require("../../../lib/rules/regexp-header");
+const RuleTester = require("eslint").RuleTester;
 
 const ruleTester = new RuleTester();
-ruleTester.run('regexp-header', rule, {
+ruleTester.run("regexp-header", rule, {
     valid: [
         {
-            code: '//\n// Copyright 2021\n// My Company\n//\n',
-            options: [{ headerFile: 'tests/support/copyright.txt' }],
+            code: "//\n// Copyright 2021\n// My Company\n//\n",
+            options: [{
+                header: "//\n// Copyright \\d{4}\n// My Company\n//"
+            }],
+        },
+        {
+            code: "//\n// Copyright 2021\n// My Company\n//\n",
+            options: [{ headerFile: "tests/support/copyright.txt" }],
         }
     ],
-    invalid: []
+    invalid: [
+        {
+            code: "//\n// Copyright 2021-02-01\n// My Company\n//\n",
+            options: [{
+                header: "//\n" +
+                    "// Copyright \\d{4}\n" +
+                    "// My Company\n" +
+                    "//"
+            }],
+            errors: [
+                { message: "incorrect header" }
+            ],
+        },
+        {
+            code: "//\n// Copyright 2021-02-01\n// My Company\n//\n",
+            options: [{ headerFile: "tests/support/copyright.txt" }],
+            errors: [
+                { message: "incorrect header" }
+            ],
+        }
+    ]
 });
